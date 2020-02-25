966fdb23da236efd2b298b9632ea83b273dc8305
Iurii Kisil <Iurii_Kisil@epam.com>
2020-02-25 15:44:45 +0200

    added code

--- GraphTest/UnitTest1.cs
+++ GraphTest/UnitTest1.cs
@@ -1,15 +1,27 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace GraphTest
 {
     [TestClass]
     public class UnitTest1
     {
+        public Dictionary<string, string> points = new Dictionary<string, string>();
+        [TestInitialize]
+        
+        public void TestInitialize()
+        {
+            new GraphPage().OpenGraphPage();
+            new GraphPage().ToolTipsTextToDictionary(points);
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            
             new GraphPage().OpenGraphPage();
-            new GraphPage().CheckGraph();
+            //new GraphPage().CheckGraph(points);
+            Assert.IsTrue(new GraphPage().CheckGraph(points));
         }
     }
 }
