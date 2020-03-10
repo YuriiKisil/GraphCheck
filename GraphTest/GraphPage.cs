4959ce17baff11b1732d08229b0e2bf09007788f
Iurii Kisil <Iurii_Kisil@epam.com>
2020-03-10 15:42:24 +0200

    code for comparing dictionaries added

--- GraphTest/GraphPage.cs
+++ GraphTest/GraphPage.cs
@@ -14,16 +14,34 @@ namespace GraphTest
 {
     class GraphPage : BasePage
     {
-        //private const string Path = "D:\ToolTips.csv";
+        
+        [FindsBy(How = How.XPath, Using = "//button[@aria-label='Toggle visibility of Revenue']")]
+        private IWebElement Graph2;
 
-        //public Dictionary<int, string> points = new Dictionary<int, string>();
+        [FindsBy(How = How.XPath, Using = "//button[@aria-label='Toggle visibility of Highsoft employees']")]
+        private IWebElement Graph3;
+
+        [FindsBy(How = How.CssSelector, Using = "g.highcharts-tooltip text")]
+        private IWebElement ToolTipText;
+
+      
+
         private void TurnOffGraph2and3()
         {
-            driver.FindElement(By.XPath("//button[@aria-label='Toggle visibility of Revenue']")).Click();
+            Graph2.Click();
             Thread.Sleep(200);
-            driver.FindElement(By.XPath("//button[@aria-label='Toggle visibility of Highsoft employees']")).Click();
+            Graph3.Click();
             Thread.Sleep(200);
         }
+
+        public void TurnOffAllflags()
+        {
+            IWebElement flag1 = driver.FindElement(By.CssSelector("g.highcharts-markers.highcharts-series-6.highcharts-flags-series.highcharts-tracker"));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].style.visibility='hidden'", flag1);
+            IWebElement flag2 = driver.FindElement(By.CssSelector("g.highcharts-markers.highcharts-series-5.highcharts-flags-series.highcharts-tracker"));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].style.visibility='hidden'", flag2);
+        }
+        
         public void OpenGraphPage()
         {
             driver.Navigate().GoToUrl("https://www.highcharts.com/demo/combo-timeline?__cf_chl_jschl_tk__=ef2cf32e3aba4d8a07eb77d98e342af21cd1fec5-1582547443-0-Ac4jESMS57PpuUpNFK0fcdcsyrL0wOaARb0xrJBPdGADsFyKrqLNLHj3pJZuGr0tV6K3ExgpJDL49Zggx_nubhVP4MxGmiWf98NAFjziC4D2-w53n4v_qpQSoqQIzA80jvqC2sLDQ97YHxJE7KH6uurPqy7ro1o3x1GLa16RX1a_Q9WIDZDLnNo4JJJ5uGGwtGEradWrGv0VZViqh9KX1xCzUQKSC79vvDF8fFxpe8lHUrwc9Q7iQoUgEeGDw2dp0mg5hJHqWDeZCp2JqkLOuFHUCTPAr-iawlO5aTClUPq83vBNsoC1CyxAks5CZr_ttVRifKhWunEBYsZow4GyvOFIPJXJXB_FcWIXnfFdf_ri");
@@ -37,44 +55,47 @@ namespace GraphTest
         }
         public void ToolTipsTextToDictionary(Dictionary<String, String> tooltips)
         {
-            //new GraphPage().TurnOffGraph2and3();
-            for (int i = 2; i < 61; i++)
+            //TurnOffAllflags();
+            //TurnOffGraph2and3();
+           
+            for (int i = 1; i < 61; i++)
             {
 
                 Actions action = new Actions(driver);
                 action.MoveToElement(driver.FindElement(By.XPath("//*[@fill='#7cb5ec'][" + i + "]"))).Perform();
-                Thread.Sleep(200);
-                IWebElement ToolTipDate = driver.FindElement(By.CssSelector("g.highcharts-tooltip text"));
-                //IWebElement ToolTipText = driver.FindElement(By.CssSelector("g.highcharts-tooltip text tspan:nth-child(6)"));
-                Console.WriteLine(ToolTipDate.Text);
-                tooltips.Add(i.ToString(), ToolTipDate.Text);
+                Thread.Sleep(20000);
+                //IWebElement ToolTipDate = driver.FindElement(By.CssSelector("g.highcharts-tooltip text"));
+                Console.WriteLine(ToolTipText.Text);
+                //tooltips.Add(i.ToString(), ToolTipDate.Text);
             }
 
 
         }
         public bool CheckGraph(Dictionary<String, String> points)
         {
-           // TurnOffGraph2and3();
+            TurnOffAllflags();
+            TurnOffGraph2and3();
             bool res = true;
-            for (int i = 7; i < 61; i++)
+            for (int i = 1; i < 61; i++)
             {
                 Actions action = new Actions(driver);
                 action.MoveToElement(driver.FindElement(By.XPath("//*[@fill='#7cb5ec'][" + i + "]"))).Perform();
                 Thread.Sleep(200);
                 IWebElement ToolTipDate = driver.FindElement(By.CssSelector("g.highcharts-tooltip text"));
+
                 Console.WriteLine(ToolTipDate.Text);
-            
-                //int key = i - 1;
-                //if (points[key.ToString()] != ToolTipDate.Text)
-                //{
-                //    res = false;
-                //    break;
-                //}
+
+               
+                if (points[i.ToString()] != ToolTipDate.Text)
+                {
+                    res = false;
+                    break;
+                }
 
             }
             return res;
+
           
-            driver.Quit();
 
         }
     }
--- GraphTest/UnitTest1.cs
+++ GraphTest/UnitTest1.cs
@@ -7,34 +7,24 @@ namespace GraphTest
     public class UnitTest1
     {
         public Dictionary<string, string> Expectedpoints = new Dictionary<string, string>();
-        public Dictionary<string, string> Actualpoints = new Dictionary<string, string>();
+      //  public Dictionary<string, string> Actualpoints = new Dictionary<string, string>();
         public bool result = true;
-        [TestInitialize]
+        private object driver;
 
+        [TestInitialize]
 
         public void TestInitialize()
         {
             new GraphPage().OpenGraphPage();
-            // new GraphPage().ToolTipsTextToDictionary(points); 
             Expectedpoints = new GraphPage().GetDataForTest();
-        }
+            new GraphPage().TurnOffAllflags();
+           // new GraphPage().ToolTipsTextToDictionary(Actualpoints);
+        }   
 
         [TestMethod]
         public void TestMethod1()
-        {
-            new GraphPage().ToolTipsTextToDictionary(Actualpoints);
-            foreach (KeyValuePair<string, string>point  in Actualpoints)
-            {
-                
-                if (point.Key != Expectedpoints[point.Key])
-                {
-                    result = false;
-                    break;
-                }
-            }
-            //new GraphPage().OpenGraphPage();
-            //new GraphPage().CheckGraph(points);
-            // bool res = new GraphPage().CheckGraph(points);
+        {    
+            result = new GraphPage().CheckGraph(Expectedpoints);
             Assert.IsTrue(result);
         }
     }
